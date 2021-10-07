1281852e59f49ea815c7fa0faf54600b04125a03
pisuttun <pisuttun@gmail.com>
2021-10-07 23:33:34 +0700

    Roughly add picture

--- src/pages/exhibition/index.tsx
+++ src/pages/exhibition/index.tsx
@@ -1,8 +1,20 @@
 import 'App.css'
+import choice1 from 'assets/pic/Activity1.png'
+import choice2 from 'assets/pic/Activity2.png'
+import choice3 from 'assets/pic/Activity3.png'
 
 function Exhibition() {
   return (
-    <p>Exhibition page?</p>
+    <div>
+      <div style={{display:'flex',  justifyContent:'center', alignItems:'center', marginTop:'122px'}}>
+        <img src={choice1} alt = "choice1" width = "163px" style={{marginRight:'38px'}} ></img>
+        <img src={choice2} alt = "choice2" width = "163px"></img>
+      </div>
+
+      <div style={{display:'flex',  justifyContent:'center', alignItems:'center', marginTop:'100px'}}>
+        <img src={choice3} alt = "choice3" width = "163px"></img>
+      </div>
+    </div>
   )
 }
 
