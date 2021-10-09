a51a25c0744fdbe78e3c26b1a3d5b292fa7ab4bc
PKhing <apimrak@gmail.com>
2021-10-09 19:16:59 +0700

    ✨ Implement question component

---
+++ src/pages/game/components/Question/styled.ts
@@ -0,0 +1,10 @@
+import { StyledText } from 'common/components/Typography'
+import { styled } from 'common/config'
+
+export const QuestionContainer = styled(StyledText, {
+  background:
+    'linear-gradient(90deg, rgba(206, 137, 75, 0) 0%, #CE894B 48.25%, rgba(206, 137, 75, 0) 100%)',
+  width: '80%',
+  color: 'white',
+  padding: '8px 0',
+})
