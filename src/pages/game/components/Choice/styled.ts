27cb38fc06cdfa1edc84cbb812207536947f737e
PKhing <apimrak@gmail.com>
2021-10-09 18:55:43 +0700

    ♻️ Refactor Choice component

---
+++ src/pages/game/components/Choice/styled.ts
@@ -0,0 +1,16 @@
+import { styled } from 'common/config'
+
+export const StyledButton = styled('button', {
+  fontSize: '$5',
+  fontWeight: '$regular',
+  fontFamily: '$ChulaCharas',
+  height: '40px',
+  width: '80%',
+  border: '4px solid #8A5627',
+  borderRadius: '5px',
+  boxShadow: '2px 2px 4px 0px #00000040',
+  cursor: 'pointer',
+  boxSizing: 'border-box',
+  color: '$black10',
+  backgroundColor: '#02030380',
+})
