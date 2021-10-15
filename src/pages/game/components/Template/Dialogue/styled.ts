527f38f57881854e42fcac48e292a7f0eac8b3dc
miello <partearth1212@gmail.com>
2021-10-15 23:15:00 +0700

    🐛 Fix padding dialogue problem

---
+++ src/pages/game/components/Template/Dialogue/styled.ts
@@ -0,0 +1,9 @@
+import { styled } from 'common/config'
+
+export const DialogueContainer = styled('div', {
+  width: '100%',
+  paddingBottom: '80px',
+  display: 'flex',
+  justifyContent: 'center',
+  background: 'linear-gradient(0deg, #292929 0%, rgba(41, 41, 41, 0) 100%)',
+})
