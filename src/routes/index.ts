5de1a951d6570602a346816bc643b5a01293011f
miello <partearth1212@gmail.com>
2021-10-15 23:08:27 +0700

    💚 Fix build failed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { RouteProps } from 'react-router-dom'
 import Example from 'pages/example'
-import Game from 'pages/game'
 import ComponentsDemo from 'pages/componentsDemo'
 import PixiTesting from 'pages/pixi'
 
