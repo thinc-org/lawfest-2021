18f876a3e5e4fb40173e85b0b70db372f510b11e
miello <partearth1212@gmail.com>
2021-10-16 10:40:56 +0700

    💄 Make the dialogue component near to bottom

--- src/pages/game/components/Template/Dialogue/styled.ts
+++ src/pages/game/components/Template/Dialogue/styled.ts
@@ -2,7 +2,7 @@ import { styled } from 'common/config'
 
 export const DialogueContainer = styled('div', {
   width: '100%',
-  paddingBottom: '80px',
+  paddingBottom: '40px',
   display: 'flex',
   justifyContent: 'center',
   background: 'linear-gradient(0deg, #292929 0%, rgba(41, 41, 41, 0) 100%)',
