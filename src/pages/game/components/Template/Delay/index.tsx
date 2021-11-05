dce400c0e684c5ca214684ad10cd51de0cb4291e
miello <partearth1212@gmail.com>
2021-11-05 11:16:13 +0700

    💄 Hide img when src is not provided

--- src/pages/game/components/Template/Delay/index.tsx
+++ src/pages/game/components/Template/Delay/index.tsx
@@ -14,7 +14,7 @@ function DelayTransition(props: IDelayTransitionProps) {
 
   return (
     <RootContainer css={{ flexDirection: 'column' }}>
-      <img src={img} alt={alt} style={{ marginBottom: '20px' }} />
+      {img && <img src={img} alt={alt} style={{ marginBottom: '20px' }} />}
       <StyledText
         css={{ maxWidth: '300px', color: textColor }}
         mobileVariant="title2"
