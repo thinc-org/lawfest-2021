aa64f7303861f756502608b7b9a74887827521e8
miello <partearth1212@gmail.com>
2021-11-06 21:29:40 +0700

    💄 Renovate credit scene to be scrolling until end

--- src/pages/game/components/Template/Credit/index.tsx
+++ src/pages/game/components/Template/Credit/index.tsx
@@ -1,55 +1,110 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { StyledText } from 'common/components/Typography'
 import { RootContainer } from '../Container'
-import { FadeContainer } from './styled'
+import { FadeContainer, ScrollContainer } from './styled'
 import CREDIT from 'common/constant/Scene/Credit'
 import { ICreditProps } from './types'
+import Candle from 'assets/images/candle.svg'
+import TextWithLine from 'common/components/TextWithLine'
 
 function Credit(props: ICreditProps) {
-  const [state, setState] = useState(0)
   const { onFinish } = props
+  const [isStart, setStart] = useState(false)
+  const [showSkip, setShowSkip] = useState(false)
+
+  const handleOnFinish = useCallback(() => {
+    onFinish({})
+  }, [onFinish])
 
   useEffect(() => {
-    let nowState = 0
-    const interval = window.setInterval(() => {
-      if (nowState === 2) {
-        onFinish({})
-        return
-      }
-      setState(++nowState)
-    }, 4000)
+    window.setTimeout(() => {
+      setStart(true)
+    }, 2500)
 
-    return () => {
-      window.clearInterval(interval)
-    }
+    window.setTimeout(() => {
+      setShowSkip(true)
+    }, 5000)
   }, [onFinish])
 
   const generateCredit = () => {
     return CREDIT.map((val, idx) => {
       return (
-        <FadeContainer show={state === idx}>
-          <StyledText mobileVariant="title1" css={{ color: '$secondary900' }}>
-            {val.topic}
-          </StyledText>
-          {val.name.map((_val) => {
-            return (
-              <FadeContainer
-                show={true}
-                css={{ marginTop: '10px', width: '100%' }}
-              >
-                <StyledText mobileVariant="title2">{_val.thai}</StyledText>
-                <StyledText mobileVariant="title2">{_val.eng}</StyledText>
-              </FadeContainer>
-            )
-          })}
-        </FadeContainer>
+        <ScrollContainer
+          css={{
+            top: `${(idx + 1) * 100}%`,
+            bottom: `${(idx + 2) * 100}%`,
+          }}
+          start={isStart}
+        >
+          <FadeContainer show={true}>
+            <StyledText mobileVariant="title1" css={{ color: '$secondary900' }}>
+              {val.topic}
+            </StyledText>
+            {val.name.map((_val) => {
+              return (
+                <FadeContainer
+                  show={true}
+                  css={{ marginTop: '10px', width: '100%' }}
+                >
+                  <StyledText mobileVariant="title2">{_val.thai}</StyledText>
+                  <StyledText mobileVariant="title2">{_val.eng}</StyledText>
+                </FadeContainer>
+              )
+            })}
+          </FadeContainer>
+        </ScrollContainer>
       )
     })
   }
 
   return (
-    <RootContainer css={{ flexDirection: 'column' }}>
+    <RootContainer
+      css={{
+        flexDirection: 'column',
+        overflow: 'hidden',
+        position: 'relative',
+      }}
+    >
+      <ScrollContainer
+        css={{ top: '0%', bottom: '100%' }}
+        start={isStart}
+        onAnimationEnd={() => {
+          handleOnFinish()
+        }}
+      >
+        <img
+          src={Candle}
+          alt="candle"
+          style={{
+            marginBottom: '20px',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+        />
+        <StyledText
+          css={{ maxWidth: '300px', color: '#683F19' }}
+          mobileVariant="title2"
+        >
+          {'แด่ผู้สร้างความหวังให้งานนี้'}
+        </StyledText>
+      </ScrollContainer>
       {generateCredit()}
+      <RootContainer
+        css={{
+          cursor: showSkip ? 'pointer' : 'default',
+          position: 'absolute',
+          width: '100%',
+          transition: 'opacity 1s linear',
+          opacity: showSkip ? 1 : 0,
+        }}
+        onClick={() => {
+          if (showSkip) handleOnFinish()
+        }}
+      >
+        <TextWithLine css={{ position: 'absolute', bottom: '70px' }}>
+          <StyledText mobileVariant="button">แตะเพื่อข้าม</StyledText>
+        </TextWithLine>
+      </RootContainer>
     </RootContainer>
   )
 }
